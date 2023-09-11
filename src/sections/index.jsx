f7a4852881b3dd0a9a485e00c024e0ad2782ff55
EnhaoSun <enhao.sun@outlook.com>
2023-09-11 16:07:06 +0800

    add project

---
+++ src/sections/index.jsx
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import CustomerReview from "./CustomerReview";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import SuperQuality from "./SuperQuality";
+
+export {
+	Hero,
+	PopularProducts,
+	CustomerReview,
+	Subscribe,
+	Footer,
+	Services,
+	SpecialOffer,
+	SuperQuality,
+};
